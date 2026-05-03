83e11c803b62c670f17f60ea0627f9acfd90df24
Victor Hugo <victorhugo.prog@gmail.com>
2026-05-03 06:24:20 -0300

    feat: implement data retrieval using Statement and ResultSet

---
+++ JDBC/jdbc2/source/app/Program.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("ID") + ", " + rs.getString("Name"));
+            }
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResulSet(rs);
+            DB.closeConnection();
+        }
+    }
+}
