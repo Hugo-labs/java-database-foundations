5e140169222b82e5b1e7f06d28dc6b6ecda4b72e
Victor Hugo <victorhugo.prog@gmail.com>
2026-06-02 21:57:12 -0300

    feat: update implementation

--- JDBC/demo-dao-jdbc/source/model/dao/impl/SellerDaoJDBC.java
+++ JDBC/demo-dao-jdbc/source/model/dao/impl/SellerDaoJDBC.java
@@ -69,8 +69,28 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller seller) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+         PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                "UPDATE seller "
+                + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                + "WHERE ID = ? "
+            );
+            st.setString(1, seller.getName());
+            st.setString(2, seller.getEmail());
+            st.setDate(3, java.sql.Date.valueOf(seller.getDate()));
+            st.setDouble(4, seller.getBaseSalary());
+            st.setInt(5, seller.getDepartment().getId());
+            st.setInt(6, seller.getId());
+
+            st.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
